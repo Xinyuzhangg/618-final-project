2b4d14716c715cb96ce94864839360fdfe6f1391
zhi lin <zhilin@macbook-air.myfiosgateway.com>
2022-04-10 01:21:36 -0400

    framework

---
+++ compute.cpp
@@ -0,0 +1,18 @@
+// linear hash
+#include "include/rigtorp/HashMap.h"
+
+// hashtable worker node
+// async receive signal from master
+void compute_hashWorker(int procID, int nproc){
+
+}
+
+// hashtable master node
+void compute_hashMaster(int procID, int nproc){
+
+}
+
+// parallel algorithm caller
+void compute_caller(int procID, int nproc){
+
+}
